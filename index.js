950da7e814b961189f618dabf6dfd8a5cd834c8a
anabergese <72303889+anabergese@users.noreply.github.com>
2020-11-24 22:07:02 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+
+$("#education .text1").hide();
+
+$(".title1").on("click", function() {
+  $("#education .text1").slideToggle( "slow", "linear" );
+});
+
+
+
+$("#education .text2").hide();
+
+$(".title2").on("click", function() {
+  $("#education .text2").slideToggle( "slow", "linear" );
+});
+
+
+$("#education .text3").hide();
+
+$(".title3").on("click", function() {
+  $("#education .text3").slideToggle( "slow", "linear" );
+});
+
+
+
+$("#education .text4").hide();
+
+$(".title4").on("click", function() {
+  $("#education .text4").slideToggle( "slow", "linear" );
+});
